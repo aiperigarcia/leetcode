2d704422b0078dafbd2b98712f79a127dd2aae74
aiperieldar <aiperieldar.dev@gmail.com>
2020-03-31 21:04:22 -0400

    Initial Commit

---
+++ IPaddress/main.js
@@ -0,0 +1,8 @@
+/* Given a valid (IPv4) IP address, return a defanged version of that IP address.
+
+A defanged IP address replaces every period "." with "[.]". */
+
+var defangIPaddr = function(address) {
+  let newAddress = address.split('.').join('[.]')
+   return newAddress;
+};

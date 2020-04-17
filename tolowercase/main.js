324d23e7ef9e930dfa3d6e5d1d4499a1cf2ae029
aiperieldar <aiperieldar.dev@gmail.com>
2020-04-17 08:53:33 -0400

    Initial Commit

--- tolowercase/main.js
+++ tolowercase/main.js
@@ -1,4 +1,4 @@
-//Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+//Return the same string in lowercase.
 
 var toLowerCase = function(str) {
     return str.toLowerCase()

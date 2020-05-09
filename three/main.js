a128fa743b972a1bfb37aa055f359493279c1bff
aiperieldar <aiperieldar.dev@gmail.com>
2020-05-08 20:22:50 -0400

    Initial Commit

---
+++ three/main.js
@@ -0,0 +1,17 @@
+/* A trick I learned in elementary school to determine whether or not a number was divisible by three is to add all of the integers in the number together and to divide the resulting sum by three. If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
+
+Given a series of numbers as a string, determine if the number represented by the string is divisible by three.
+
+You can expect all test case arguments to be strings representing values greater than 0. */
+
+function divisibleByThree(str){
+  let sum = 0
+  for(var i = 0; i < str.length; i++){
+    sum += parseInt(str[i])
+  }
+     if(sum % 3 === 0){
+      return true
+    } else{
+    return false
+    }
+}

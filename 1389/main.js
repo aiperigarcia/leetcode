da5d2d85e781924fc3d487bd8952ed8e0540c0a0
aiperieldar <aiperieldar.dev@gmail.com>
2020-05-20 11:33:22 -0400

    Initial Commit

---
+++ 1389/main.js
@@ -0,0 +1,66 @@
+/*
+
+Given two arrays of integers nums and index. Your task is to create target array under the following rules:
+
+Initially target array is empty.
+From left to right read nums[i] and index[i], insert at index index[i] the value nums[i] in target array.
+Repeat the previous step until there are no elements to read in nums and index.
+Return the target array.
+
+It is guaranteed that the insertion operations will be valid.
+
+
+
+Example 1:
+
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+Explanation:
+nums       index     target
+0            0        [0]
+1            1        [0,1]
+2            2        [0,1,2]
+3            2        [0,1,3,2]
+4            1        [0,4,1,3,2]
+Example 2:
+
+Input: nums = [1,2,3,4,0], index = [0,1,2,3,0]
+Output: [0,1,2,3,4]
+Explanation:
+nums       index     target
+1            0        [1]
+2            1        [1,2]
+3            2        [1,2,3]
+4            3        [1,2,3,4]
+0            0        [0,1,2,3,4]
+Example 3:
+
+Input: nums = [1], index = [0]
+Output: [1]
+
+*/
+
+/*
+p array of numbers
+r array of numbers
+e
+Input: nums = [0,1,2,3,4], index = [0,1,2,2,1]
+Output: [0,4,1,3,2]
+p:
+
+1.create an empty array
+2.iterate through nums array and index array
+2. get index of index[i] and equal it to value of nums[i]
+3. if new index[i] has two number, put biggest first.
+4. push new value to empty array
+5. return new array
+
+*/
+
+var createTargetArray = function(nums, index) {
+    let target=[];
+    for (let i=0; i<nums.length; i++) {
+        target.splice(index[i], 0, nums[i]);
+    }
+    return target;
+};

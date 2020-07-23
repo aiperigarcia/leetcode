48d5433d1958b0a351bf201903e3edc7d2bf1519
aiperieldar <aiperieldar.dev@gmail.com>
2020-07-23 17:41:00 -0400

    Initial Commit

---
+++ 217/main.js
@@ -0,0 +1,29 @@
+/*
+Given an array of integers, find if the array contains any duplicates.
+
+Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+Example 1:
+
+Input: [1,2,3,1]
+Output: true
+Example 2:
+
+Input: [1,2,3,4]
+Output: false
+Example 3:
+
+Input: [1,1,1,
+*/
+
+var containsDuplicate = function(nums) {
+    let array = {};
+    for(let i = 0; i < nums.length; i++) {
+        if (array[nums[i]]) {
+            return true;
+        }else{
+            array[nums[i]] = true;
+        }
+    }
+    return false;
+};

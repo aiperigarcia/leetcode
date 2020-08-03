756c71b488960ad05866eefbed9425b68257d1c3
aiperieldar <aiperieldar.dev@gmail.com>
2020-08-02 21:55:11 -0400

    Initial Commit

---
+++ 1512/main.js
@@ -0,0 +1,36 @@
+/*
+Given an array of integers nums.
+
+A pair (i,j) is called good if nums[i] == nums[j] and i < j.
+
+Return the number of good pairs.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1,1,3]
+Output: 4
+Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+Example 2:
+
+Input: nums = [1,1,1,1]
+Output: 6
+Explanation: Each pair in the array are good.
+Example 3:
+
+Input: nums = [1,2,3]
+Output: 0
+*/
+
+var numIdenticalPairs = function(nums) {
+    let count = 0
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(nums[i] == nums[j] && i < j){
+            count++
+         }
+       }
+    }
+    return count
+};

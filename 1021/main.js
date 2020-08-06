9d54b64acfb1961b7c978be715062480215ee91b
aiperieldar <aiperieldar.dev@gmail.com>
2020-08-06 13:53:03 -0400

    Initial Commit

---
+++ 1021/main.js
@@ -0,0 +1,45 @@
+/*
+A valid parentheses string is either empty (""), "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.  For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+A valid parentheses string S is primitive if it is nonempty, and there does not exist a way to split it into S = A+B, with A and B nonempty valid parentheses strings.
+
+Given a valid parentheses string S, consider its primitive decomposition: S = P_1 + P_2 + ... + P_k, where P_i are primitive valid parentheses strings.
+
+Return S after removing the outermost parentheses of every primitive string in the primitive decomposition of S.
+
+
+
+Example 1:
+
+Input: "(()())(())"
+Output: "()()()"
+Explanation:
+The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+Example 2:
+
+Input: "(()())(())(()(()))"
+Output: "()()()()(())"
+Explanation:
+The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+*/
+
+var removeOuterParentheses = function(S) {
+    let stack = 0;
+    let result = [];
+    for(let i = 0; i < S.length; i++){
+        if (S[i] === '('){
+        	if (stack !== 0){
+        		result.push('(');
+        	}
+            stack++;
+        } else {
+            stack--;
+            if (stack !== 0){
+            	result.push(')');
+            }
+        }
+    }
+    return result.join('')
+};

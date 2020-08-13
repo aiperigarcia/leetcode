58b1ef13bcffaa0690f7be9b2bc0f89c99230c27
aiperieldar <aiperieldar.dev@gmail.com>
2020-08-12 21:26:48 -0400

    Initial Commit

---
+++ 1351/main.js
@@ -0,0 +1,24 @@
+// Given a m * n matrix grid which is sorted in non-increasing order both row-wise and column-wise.
+//
+// Return the number of negative numbers in grid.
+//
+//
+//
+// Example 1:
+//
+// Input: grid = [[4,3,2,-1],[3,2,1,-1],[1,1,-1,-2],[-1,-1,-2,-3]]
+// Output: 8
+// Explanation: There are 8 negatives number in the matrix.
+// Example 2:
+//
+// Input: grid = [[3,2],[1,0]]
+// Output: 0
+// Example 3:
+//
+// Input: grid = [[1,-1],[-1,-1]]
+// Output: 3
+
+
+var countNegatives = function (grid) {
+	return grid.reduce((a, b) => a + b.filter(num => num < 0).length, 0);
+};
